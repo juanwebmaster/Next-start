6b9b82dcbc7dffc052235ce33104f0795a4745a4
juanwebmaster <darius.ball.dev.@hotmail.com>
2020-04-12 11:52:09 -0700

    Feat: Fetching Data for Pages implemented

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,22 +1,41 @@
-import Layout from './components/MyLayout'
-import Link from 'next/link';
+import Layout from "./components/MyLayout";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
+const PostLink = props => {
+  console.log(props)
+  return (
+    <Link href="/p/[id]" as={`/p/${props.props.id}`}>
+      <a>{props.props.name}</a>
+    </Link>
+  )
+}
 
-const PostLink = props => (
-  <Link href="/p/[id]" as={`/p/${props.id}`}>
-    <a>{props.id}</a>
-  </Link>
-)
-
-export default function Blog() {
+const Blog = props => {
+  
   return (
     <Layout>
-      <h1>My Blog</h1>
+      <h1>Batman TV Shows</h1>
       <ul>
-        <li><PostLink id="hello-next" /></li>
-        <li><PostLink id="learn-next" /></li>
-        <li><PostLink id="deploy-next" /></li>
+        {props.shows.map(show => (
+          <li key={show.id}>
+            <PostLink props={show} />
+          </li>
+        ))}
       </ul>
     </Layout>
   );
 }
+
+Blog.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  }
+}
+
+export default Blog;
