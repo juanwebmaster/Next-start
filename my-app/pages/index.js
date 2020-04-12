c1da80f794c291bb649189e18ef01f16d75a1dab
juanwebmaster <darius.ball.dev.@hotmail.com>
2020-04-12 12:50:50 -0700

    Feat: API implemented

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -2,6 +2,7 @@ import Layout from "./components/MyLayout";
 import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 import useSWR from 'swr';
+import { useRouter } from 'next/router';
 
 const PostLink = props => {
   console.log(props)
@@ -12,20 +13,41 @@ const PostLink = props => {
   )
 }
 
-const Blog = props => {
+function fetcher(url) {
+  return fetch(url).then(r => r.json());
+}
+
+const Blog = () => {
+  const { query } = useRouter();
+  const { data, error } = useSWR(
+    `/api/randomQuote${query.author ? '?author=' + query.author : ''}`,
+    fetcher
+  )
+  const author = data?.author;
+  let quote = data?.quote;
   
+  if (!data) quote = 'Loading...';
+  if (error) quote = 'Failed to fetch the quote.';
+
   return (
-    <Layout>
-      <h1>Batman TV Shows</h1>
-      <ul>
-        {props.shows.map(show => (
-          <li key={show.id}>
-            <PostLink props={show} />
-          </li>
-        ))}
-      </ul>
-    </Layout>
-  );
+    <main className="center">
+      <div className="quote">{quote}</div>
+      {author && <span className="author">- {author}</span>}
+    </main>
+  )
+
+  // return (
+  //   <Layout>
+  //     <h1>Batman TV Shows</h1>
+  //     <ul>
+  //       {props.shows.map(show => (
+  //         <li key={show.id}>
+  //           <PostLink props={show} />
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   </Layout>
+  // );
 }
 
 Blog.getInitialProps = async function() {
